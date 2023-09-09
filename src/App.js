82d39f6f0fd9f798fd57922003b42ee120844baa
Souvik chakraborty <22souvik22@gmail.com>
2023-09-09 17:19:47 +0530

    Final touch done

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ import './App.css';
 
 export default function App() {
     const [notes, setNotes] = React.useState([])
-
     const [currentNoteId, setCurrentNoteId] = React.useState("")
-    
+    const [tempNoteText, setTempNoteText] = React.useState("")
+
     const findCurrentNote = notes.find(note => note.id === currentNoteId) || notes[0]
+    const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt)
+
 
     React.useEffect(() => {
-        const unsubscribe = onSnapshot(notescollection ,(snapshot)=>{
+        const unsubscribe = onSnapshot(notescollection, (snapshot) => {
             const notesArr = snapshot.docs.map(doc => ({
                 ...doc.data(),
                 id: doc.id
@@ -22,26 +24,46 @@ export default function App() {
             setNotes(notesArr)
         })
         return unsubscribe
-    },[])
-
+    }, [])
 
     React.useEffect(() => {
         if (!currentNoteId) {
             setCurrentNoteId(notes[0]?.id)
         }
     }, [notes])
-    
+
+    React.useEffect(() => {
+        if (findCurrentNote) {
+            setTempNoteText(findCurrentNote.body)
+        }
+    }, [findCurrentNote])
+
+    React.useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (tempNoteText !== findCurrentNote.body) {
+                updateNote(tempNoteText)
+            }
+        }, 500)
+        return () => clearTimeout(timeoutId)
+    }, [tempNoteText])
+
     async function createNewNote() {
         const newNote = {
-            body: "# Type your markdown note's title here"
+            body: "# Type your markdown note's title here",
+            createdAt: Date.now(),
+            updatedAt: Date.now()
         }
         const newNoteRef = await addDoc(notescollection, newNote)
         setCurrentNoteId(newNoteRef.id)
     }
-    
+
     async function updateNote(text) {
         const docRef = doc(db, "notes", currentNoteId)
-        await setDoc(docRef, { body: text }, { merge: true })
+        await setDoc(
+            docRef,
+            { body: text, updatedAt: Date.now() },
+            { merge: true }
+        )
     }
 
     async function deleteNote(noteId) {
@@ -49,41 +71,41 @@ export default function App() {
         await deleteDoc(docRef)
     }
 
-    
+
     return (
         <main>
-        {
-            notes.length > 0 
-            ?
-            <Split 
-                sizes={[30, 70]} 
-                direction="horizontal" 
-                className="split"
-            >
-                <Sidebar
-                    notes={notes}
-                    currentNote={findCurrentNote}
-                    setCurrentNoteId={setCurrentNoteId}
-                    newNote={createNewNote}
-                    deleteNote={deleteNote}
-                />
-                    <Editor 
-                        currentNote={findCurrentNote} 
-                        updateNote={updateNote} 
-                    />
-            </Split>
-            :
-            <div className="no-notes">
-                <h1>You have no notes</h1>
-                <button 
-                    className="first-note" 
-                    onClick={createNewNote}
-                >
-                    Create one now
-                </button>
-            </div>
-            
-        }
+            {
+                notes.length > 0
+                    ?
+                    <Split
+                        sizes={[30, 70]}
+                        direction="horizontal"
+                        className="split"
+                    >
+                        <Sidebar
+                            notes={sortedNotes}
+                            currentNote={findCurrentNote}
+                            setCurrentNoteId={setCurrentNoteId}
+                            newNote={createNewNote}
+                            deleteNote={deleteNote}
+                        />
+                        <Editor
+                            tempNoteText={tempNoteText}
+                            setTempNoteText={setTempNoteText}
+                        />
+                    </Split>
+                    :
+                    <div className="no-notes">
+                        <h1>You have no notes</h1>
+                        <button
+                            className="first-note"
+                            onClick={createNewNote}
+                        >
+                            Create one now
+                        </button>
+                    </div>
+
+            }
         </main>
     )
 }
